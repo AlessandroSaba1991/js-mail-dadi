4bfb528e650c69a9e146b1313c7382b2d55f1d8d
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-03-29 14:10:08 +0200

    exercise gioco dei dadi complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,31 @@ for (let i = 0; i < mail_autorizzate.length; i++) {
 
 //stampo la risposta
 console.log(text);
+
+/* 
+CONSEGNA 
+Gioco dei dadi
+Generare un numero random da 1 a 6, 
+sia per il giocatore sia per il computer. 
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda: 
+Che ci sia un array da qualche parte? 
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
+
+//Genero numero random per l'utente e lo mostro
+const num_utente = Math.floor(Math.random() * 6) + 1;
+console.log(num_utente);
+
+//Genero numero random per il computer e lo mostro
+const num_computer = Math.floor(Math.random() * 6) + 1;
+console.log(num_computer);
+
+//stabilisco vincitore
+if (num_utente > num_computer) {
+    console.log('Sei il vincitore');
+} else if (num_utente == num_computer) {
+    console.log('Avete pareggiato');
+} else {
+    console.log('Hai perso');
+}
